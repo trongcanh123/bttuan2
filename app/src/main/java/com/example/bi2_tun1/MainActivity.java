b0b6b096906a3bbd98dfcfe4cfd0cc39f4801ca1
trongcanh123 <canhphan2002@gmail.com>
2022-08-27 11:40:28 +0700

    main
    version 1

---
+++ app/src/main/java/com/example/bi2_tun1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.bi2_tun1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button chuyenbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        chuyenbtn = findViewById(R.id.button);
+        chuyenbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
